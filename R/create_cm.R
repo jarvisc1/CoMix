d50c11eb2d16ca04b0f6972bfb4412bd245ac399
jarvisc1 <christopher.jarvis@lshtm.ac.uk>
2020-04-20 19:51:23 +0000

    Reduce functions and document

--- R/create_cm.R
+++ R/create_cm.R
@@ -12,25 +12,7 @@
 #' @param country limit to one or more countries; if not given, will use all countries in the survey; these can be given as country names or 2-letter (ISO Alpha-2) country codes
 #' @param age_limits lower limits of the age groups over which to construct the matrix
 #' @param symmetric whether to make matrix symmetric
-#' @param filter whether to filter the data default is FALSE, need to be TRUE for filter variables to work
-#' @param phys_contact filter by physical or non-phyisical contact
-#' @param home  filter by contacts in the home
-#' @param other_house filter to contacts in other homes
-#' @param work filter by contacts at work
-#' @param school fitler by contacts at school
-#' @param gender filter by gender
-#' @param hhm_quarantine filter by contacts with household members in quarantine
-#' @param hhm_isolate filter by contacts with household members in isolation
-#' @param hhm_limit_work filter by contacts with household members who have been asked to  limit work
-#' @param hhm_limit_school filter by contacts with household members who have have been asked to limit school
-#' @param hhm_work_closed filter by contacts with household members who have had their work closed
-#' @param part_quarantine filter by participants in quarantine
-#' @param part_isolate filter by participants in isolation
-#' @param part_limit_work filter by participants who have been asked to limit work
-#' @param part_limit_school filter by participants who have been asked to limit school
-#' @param part_work_closed filter by participants who have had their work closed
-#' @param part_covid_test_results filter by participants with a positive covid test result
-#' @param part_covid_contact filter by participants who have had contact with a someone who has had covid
+#' @param filter list object for filtering the contacts matrix to work
 #' @return Either contact matrix or contact matrix and with participants information
 #'
 #' @export
--- R/create_scaling_matrices.R
+++ R/create_scaling_matrices.R
@@ -1,10 +1,22 @@
-#' Will add documentation
+#'  Creates matrices for scaling CoMix by POLYMOD
+#'
+#' Take two matrices split them if required to be of the same size and square
+#' Then take the ratio of the maximum eigenvalues
+#'
+#' @param comix_survey Survey object for comix
+#' @param polymod_survey Survey object for POLYMOD
+#' @param matrices_path File path where the matrices should be saved
+#' @param file_name Name of file for multiple matrices to be save into.
+#' @param split2 logical parameter, defaulted to FALSE, for splitting the second matrix
+#' @param observed logical parameter, defaulted to FALSE, for saving the observed contact matrices function
+#' @export
 #' @export
 #'
 create_scaling_matrices <- function(comix_survey, polymod_survey, nboots,
                                     matrices_path, file_name,
                                     age_limits = c(0, 5, 18, 30, 40, 50, 60, 70),
-                                    age_limits_sym = c(18, 30, 40, 50, 60, 70)) {
+                                    age_limits_sym = c(18, 30, 40, 50, 60, 70),
+                                    observed = FALSE) {
 
   ## We do not have data on participants below 18 so need to have lower limit
   ## Of 18 to do the inputed and have a symmetric matrix
@@ -17,38 +29,55 @@ create_scaling_matrices <- function(comix_survey, polymod_survey, nboots,
 
   ## Create location specific contact matrices
   ## Use age_limits_sym for COMIX matrices to match POLYMOD dimensions
-  comix_cm_school <- cm_filter(survey = comix_survey, age_limits = age_limits_sym,
-                             symmetric = TRUE, boots = nboots ,
-                             filter_text = list(cnt_school = "Yes"))
 
-  comix_cm_home <- cm_filter(survey = comix_survey, age_limits = age_limits_sym,
-                             symmetric = TRUE, boots = nboots,
-                             filter_text = list(cnt_home = "Yes"))
+  if(phys){
+    filter_comix <- list(list(phys_contact = 1, cnt_school = "Yes"),
+                         list(phys_contact = 1, cnt_home = "Yes"),
+                         list(phys_contact = 1, cnt_work = "Yes"),
+                         list(phys_contact = 1, cnt_work = "No",
+                              cnt_school = "No",
+                              cnt_home = "No"))
 
-  comix_cm_work <- cm_filter(survey = comix_survey, age_limits = age_limits_sym,
-                             symmetric = TRUE, boots = nboots,
-                             filter_text = list(cnt_work = "Yes"))
+    filter_polymod <- list(list(phys_contact = 2, cnt_school = 1),
+                           list(phys_contact = 2, cnt_home = 1),
+                           list(phys_contact = 2, cnt_work = 1),
+                           list(phys_contact = 2, cnt_work = 0,
+                                cnt_school = 0,
+                                cnt_home = 0))
+  } else{
+   filter_comix <- list(list(cnt_school = "Yes"),
+                        list(cnt_home = "Yes"),
+                        list(cnt_work = "Yes"),
+                        list(cnt_work = "No",
+                             cnt_school = "No",
+                             cnt_home = "No"))
+   filter_polymod <- list(list(cnt_school = 1),
+                          list(cnt_home = 1),
+                          list(cnt_work = 1),
+                          list(cnt_work = 0,
+                               cnt_school = 0,
+                               cnt_home = 0))
+  }
 
-  comix_cm_other <- cm_filter(survey = comix_survey, age_limits = age_limits_sym,
-                              symmetric = TRUE, boots = nboots,
-                              filter_text = list(cnt_work = "No", cnt_school = "No",
-                                                 cnt_home = "No"))
 
-  ## Repeat for Polymod
-  ## Use age_limits for POLYMOD matrices
-  polymod_cm_school <- cm_filter(survey = polymod_survey, age_limits = age_limits,
-                                 symmetric = TRUE, boots = nboots,
-                                 filter_text = list(cnt_school = 1))
-  polymod_cm_home <- cm_filter(survey = polymod_survey, age_limits = age_limits,
-                                 symmetric = TRUE, boots = nboots,
-                                 filter_text = list(cnt_home = 1))
-  polymod_cm_work <- cm_filter(survey = polymod_survey, age_limits = age_limits,
-                               symmetric = TRUE, boots = nboots,
-                               filter_text = list(cnt_work = 1))
-  polymod_cm_other <- cm_filter(survey = polymod_survey, age_limits = age_limits,
-                                symmetric = TRUE, boots = nboots,
-                                filter_text = list(cnt_work = 0, cnt_school = 0,
-                                                   cnt_home = 0))
+  comix_names <- list("comix_cm_school",
+                      "comix_cm_home",
+                      "comix_cm_work",
+                      "comix_cm_other"
+                      )
+
+  for (i in 1:length(filter_comix)){
+
+    assign(comix_names[i], cm_filter(survey = comix_survey, age_limits = age_limits_sym,
+                                    symmetric = TRUE, boots = nboots ,
+                                    filter_text = filter_comix[i])
+    )
+    assign(polymod_names[i], cm_filter(survey = polymod_survey, age_limits = age_limits,
+                                    symmetric = TRUE, boots = nboots ,
+                                    filter_text = filter_polymod[i])
+    )
+  }
+
 
   save(
       comix_cm,
@@ -62,4 +91,11 @@ create_scaling_matrices <- function(comix_survey, polymod_survey, nboots,
       polymod_cm_work,
       polymod_cm_other,
       file = file.path(matrices_path, file_name))
+
+  if (observed) {
+    saveRDS(comix_cm, file = file.path(matrices_path, "comix_cm.rds"))
+    saveRDS(polymod_cm, file = file.path(matrices_path, "polymod_cm.rds"))
+  }
+
+
 }
